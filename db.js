f16efc8d7bc1a56b3e8e1e1c8cbb53dcb89e7791
xiongjinhui <2650748689@qq.com>
2025-03-27 19:15:28 +0800

    初始化项目

---
+++ db.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  connectionLimit: 10,
+});
+
+export default db.promise();
