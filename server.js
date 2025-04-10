f6bcec01dfb51f3070631122d6d8e489224762fd
xiongjinhui <2650748689@qq.com>
2025-04-10 15:35:31 +0800

    feat: change host

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import { initDatabase } from "./initDatabase.js";
 
 const app = express();
 const port = 3001;
-const host = '0.0.0.0'; // 绑定到云服务器网络接口
 //中间件
 app.use(cors());
 app.use(express.json());
@@ -23,7 +22,7 @@ app.use("/api", protectedRoutes);
 // 先初始化数据库
 initDatabase().then(() => {
 //启动服务器
-app.listen(port, host,() => {
+app.listen(port,() => {
   console.log("服务器启动成功", `http://8.138.215.26:${port}`);
 });
 })
