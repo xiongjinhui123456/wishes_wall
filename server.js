d0d13b437c2d45916b05b6ca979291cae9822e87
xiongjinhui <2650748689@qq.com>
2025-04-10 14:52:41 +0800

    feat: change host

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import protectedRoutes from "./routes/protected.js";
 
 const app = express();
 const port = 3001;
-
+const host = '8.138.215.26'; // 绑定到云服务器网络接口
 //中间件
 app.use(cors());
 app.use(express.json());
@@ -21,6 +21,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api", protectedRoutes);
 
 //启动服务器
-app.listen(port, () => {
-  console.log("服务器启动成功", `http://localhost:${port}`);
+app.listen(port, host,() => {
+  console.log("服务器启动成功", `http://${host}:${port}`);
 });
